6b3fb585a3c270be9a0778edf07008ebd4c7d627
RishabhBoop <rishabh.tv@gmail.com>
2025-04-17 17:50:46 +0200

    removed ncurses code

--- MCT/tst.cpp
+++
@@ -1,9 +0,0 @@
-#include <ncurses.h>
-
-int main() {
-    printw("Hello, ncurses!"); // Print text to the screen
-    refresh();              // Refresh the screen
-    getch();                // Wait for user input
-    endwin();               // End ncurses mode
-    return 0;
-}
